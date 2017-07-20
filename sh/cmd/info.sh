77b37fbf92ed275d23c368b51d2de6c87e1e3a3b
Dashamir Hoxha <dashohoxha@gmail.com>
2017-07-20 10:14:53 +0200

    Improve cmd_info

--- sh/cmd/info.sh
+++ sh/cmd/info.sh
@@ -1,8 +1,39 @@
 cmd_info() {
-    cat <<_EOF
-SRC='$SRC'
-IMAGE='$IMAGE'
-CONTAINER='$CONTAINER'
-PORTS='$PORTS'
+    local format, name, time, size, status
+
+    cat <<-_EOF
+
+SETTINGS:
+    SRC:       $SRC
+    IMAGE:     $IMAGE
+    CONTAINER: $CONTAINER
+    PORTS:     $PORTS
+
 _EOF
+
+    if [[ -n $IMAGE ]]; then
+        format="{{.Repository}}:{{.Tag}},{{.CreatedSince}},{{.Size}}"
+        while IFS=, read name time size; do
+            cat <<-_EOF
+IMAGE:
+    Name:      $name
+    Created:   $time ago
+    Size:      $size
+
+_EOF
+        done < <(docker images --format "$format" | grep $IMAGE)
+    fi
+
+    if [[ -n $CONTAINER ]]; then
+        format="{{.Names}},{{.RunningFor}},{{.Status}}"
+        while IFS=, read name time status; do
+        cat <<-_EOF
+CONTAINER:
+    Name:      $name
+    Created:   $time ago
+    Status:    $status
+
+_EOF
+        done < <(docker ps -a --format "$format" | grep $CONTAINER)
+    fi
 }
