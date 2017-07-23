ec4fefa151139ddecf1e7e6078142a0fbd9b663b
Dashamir Hoxha <dashohoxha@gmail.com>
2017-07-23 08:33:03 +0200

    Improve bash-completion

--- sh/bash-completion.sh
+++ sh/bash-completion.sh
@@ -7,9 +7,45 @@
 _ds()
 {
     COMPREPLY=()   # Array variable storing the possible completions.
-    local cur=${COMP_WORDS[COMP_CWORD]}
-    local commands="init info build create config shell exec start stop restart snapshot remove help version"
-    COMPREPLY=( $(compgen -W "$commands" -- $cur) )
+    local cur=${COMP_WORDS[COMP_CWORD]}     ## $2
+    local prev=${COMP_WORDS[COMP_CWORD-1]}  ## $3
+
+    if [[ $COMP_CWORD -eq 1 ]]; then
+        local commands="version start stop restart shell exec remove"
+        commands+=" build config create help info init runcfg snapshot"
+        commands+=" $(_ds_custom_commands)"
+        COMPREPLY=( $(compgen -W "$commands" -- $cur) )
+    else
+        cmd=${COMP_WORDS[1]}
+        case $cmd in
+            init)
+                compopt -o dirnames
+                ;;
+            runcfg)
+                local cfgscripts="apache2 get-ssl-cert mount_tmp_on_ram mysql phpmyadmin set_prompt ssmtp"
+                cfgscripts+=" $(_ds_custom_cfgscripts)"
+                COMPREPLY=( $(compgen -W "$cfgscripts" -- $cur) )
+                ;;
+            *) :
+                ;;
+        esac
+    fi
+}
+
+_ds_custom_commands() {
+    local commands=""
+    source ./settings.sh
+    [[ -d $SRC/cmd/ ]] && commands=$(ls $SRC/cmd/)
+    commands="${commands//.sh/}"
+    echo $commands
+}
+
+_ds_custom_cfgscripts() {
+    local cfgscripts=""
+    source ./settings.sh
+    [[ -d $SRC/config/ ]] && cfgscripts=$(ls $SRC/config/)
+    cfgscripts="${cfgscripts//.sh/}"
+    echo $cfgscripts
 }
 
 complete -F _ds ds docker.sh src/docker.sh
