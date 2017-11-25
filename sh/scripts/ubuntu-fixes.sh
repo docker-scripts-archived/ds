573e314ca232d3ee2795782d1903c76c2f20b54d
Dashamir Hoxha <dashohoxha@gmail.com>
2017-11-25 10:37:07 +0100

    Fixing some small issues on ubuntu.

--- sh/scripts/ubuntu-fixes.sh
+++ sh/scripts/ubuntu-fixes.sh
@@ -1,6 +1,6 @@
 #!/bin/bash -x
+
 ### Fix a warning message from logrotate and rsyslog
 ### See: https://github.com/ouvrages/rsyslog/commit/ad4cc8949c9caa1e3c95389238f0b637a5325fa2
-
 sed -i /etc/logrotate.d/rsyslog \
     -e 's/invoke-rc.d rsyslog rotate/invoke-rc.d --quiet rsyslog rotate/'
