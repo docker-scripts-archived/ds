b1e323f8a6db450ea44c94ba29ba776f0d54c099
dashohoxha <dashohoxha@gmail.com>
2018-08-01 20:00:01 +0200

    Preprocess Dockerfile with m4 in order to allow including.

--- sh/cmd/build.sh
+++ sh/cmd/build.sh
@@ -7,5 +7,15 @@ _EOF
 }
 
 cmd_build() {
-    log docker build "$@" --tag=$IMAGE $APP_DIR/
+    # copy docker files to tmp/
+    # and preprocess Dockerfile
+    rm -rf tmp/
+    cp -a $APP_DIR tmp
+    m4 -I "$LIBDIR/dockerfiles" $APP_DIR/Dockerfile > tmp/Dockerfile
+
+    # build the image
+    log docker build "$@" --tag=$IMAGE tmp/
+
+    # clean up
+    rm -rf tmp/
 }
