31092ef421e2c97a401e14ef4926aae31215394c
Dashamir Hoxha <dashohoxha@gmail.com>
2018-08-14 14:38:01 +0200

    Allow dockerfiles to be included from the $APP_DIR as well

--- sh/cmd/build.sh
+++ sh/cmd/build.sh
@@ -11,7 +11,8 @@ cmd_build() {
     # and preprocess Dockerfile
     rm -rf tmp/
     cp -a $APP_DIR tmp
-    m4 -I "$LIBDIR/dockerfiles" $APP_DIR/Dockerfile > tmp/Dockerfile
+    m4 -I $APP_DIR -I "$LIBDIR/dockerfiles" \
+       $APP_DIR/Dockerfile > tmp/Dockerfile
 
     # build the image
     log docker build "$@" --tag=$IMAGE tmp/
