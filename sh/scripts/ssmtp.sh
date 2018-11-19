578b7d5506af031f4c0e0468b278f869b03963fc
dashohoxha <dashohoxha@gmail.com>
2018-11-19 19:33:28 +0100

    Prefer SMTP_SERVER over GMAIL_ADDRESS

--- sh/scripts/ssmtp.sh
+++ sh/scripts/ssmtp.sh
@@ -2,7 +2,14 @@
 
 source /host/settings.sh
 
-if [[ -n $GMAIL_ADDRESS ]]; then
+if [[ -n $SMTP_SERVER ]]; then
+    cat <<-_EOF > /etc/ssmtp/ssmtp.conf
+mailhub=$SMTP_SERVER
+rewriteDomain=${SMTP_DOMAIN:-$DOMAIN}
+UseSTARTTLS=YES
+FromLineOverride=YES
+_EOF
+elif [[ -n $GMAIL_ADDRESS ]]; then
     cat <<-_EOF > /etc/ssmtp/ssmtp.conf
 root=$GMAIL_ADDRESS
 mailhub=smtp.gmail.com:587
@@ -16,12 +23,5 @@ FromLineOverride=YES
 _EOF
     cat <<-_EOF > /etc/ssmtp/revaliases
 root:$GMAIL_ADDRESS:smtp.gmail.com:587
-_EOF
-elif [[ -n $SMTP_SERVER ]]; then
-    cat <<-_EOF > /etc/ssmtp/ssmtp.conf
-mailhub=$SMTP_SERVER
-rewriteDomain=${SMTP_DOMAIN:-$DOMAIN}
-UseSTARTTLS=YES
-FromLineOverride=YES
 _EOF
 fi
